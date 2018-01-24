6f7806a3851dd330e6369eaf819b658170a97f62
Zach Carls <messenger.7even@gmail.com>
2018-01-24 11:52:18 -0600

    Added logic for go server to edit a javascript based configuration file for the front end, and change the address and port that the frontend is looking for

--- main.go
+++ main.go
@@ -3,10 +3,14 @@ package main
 import (
 	"GenomeBustersBackend/webserver"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"os"
+	"os/exec"
 	"os/signal"
+	"runtime"
 	"strconv"
+	"strings"
 
 	"github.com/spf13/viper"
 )
@@ -21,7 +25,17 @@ func main() {
 	port := ":" + strconv.Itoa(v.GetInt("port"))
 	apiport := ":" + strconv.Itoa(v.GetInt("apiPort"))
 
-	fileServer := http.FileServer(http.Dir(v.GetString("serverRoot")))
+	fmt.Println("Rebuilding Frontend")
+	configureFrontend(v)
+	npmBuild := exec.Command("npm", "run-script", "build")
+	npmBuild.Dir = v.GetString("serverRoot")
+	npmBuild.Stderr = os.Stderr
+	if err = npmBuild.Run(); err != nil {
+		panic(err)
+	}
+	fmt.Println("Frontend rebuilt")
+
+	fileServer := http.FileServer(http.Dir(v.GetString("serverRoot") + "/build"))
 	// fh := http.Handle("/", fileServer)
 	// http.HandleFunc("/api/gene_search/", webserver.GeneSearch)
 
@@ -52,11 +66,33 @@ func main() {
 	fmt.Printf("Goodbye!\n")
 }
 
+func configureFrontend(v *viper.Viper) {
+	filePath := v.GetString("serverRoot") + "/src/config.js"
+	file, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		panic(err)
+	}
+	lines := strings.Split(string(file), "\n")
+	for i, line := range lines {
+		words := strings.Fields(line)
+
+		switch words[0] {
+		case "address:":
+			lines[i] = "address: \"" + v.GetString("apiAddress") + ":" + strconv.Itoa(v.GetInt("apiPort")) + "/api/gene_search\","
+			break
+		}
+	}
+	if err := ioutil.WriteFile(filePath, []byte(strings.Join(lines, "\n")), 0644); err != nil {
+		panic(err)
+	}
+}
+
 func initializeConfiguration() (*viper.Viper, error) {
 	v := viper.New()
-	v.SetDefault("serverRoot", "./GenomeBusters/polymorphs-frontend-master/build")
+	v.SetDefault("serverRoot", "./GenomeBusters/polymorphs-frontend-master")
 	v.SetDefault("port", 80)
 	v.SetDefault("apiPort", 8080)
+	v.SetDefault("apiAddress", "127.0.0.1")
 	err := readInConfig(v)
 	return v, err
 }
@@ -64,8 +100,10 @@ func initializeConfiguration() (*viper.Viper, error) {
 func readInConfig(v *viper.Viper) error {
 	v.SetConfigType("toml")
 	v.SetConfigName(".busted")
-	v.AddConfigPath("$XDG_CONFIG_HOME/.config/busted/")
-	v.AddConfigPath("/etc/busted/")
+	if runtime.GOOS != "windows" {
+		v.AddConfigPath("$XDG_CONFIG_HOME/.config/busted/")
+		v.AddConfigPath("/etc/busted/")
+	}
 	v.AddConfigPath(".")
 	return v.ReadInConfig()
 }
